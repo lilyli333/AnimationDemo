0cc97f1c1c5cbe08d177104aee424426a8cb28ef
jli408 <jli408@PR12.hhs.win.fuhsd.org>
2018-05-02 14:10:46 -0700

    act method functions

--- AnimationDemo/src/Mario.java
+++ AnimationDemo/src/Mario.java
@@ -9,11 +9,9 @@ public class Mario extends Sprite {
 
 	public static final int MARIO_WIDTH = 40;
 	public static final int MARIO_HEIGHT = 60;
-	private static boolean onGround;
 
 	public Mario(PImage img, int x, int y) {
 		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
-		onGround = false;
 	}
 
 	// METHODS
@@ -26,11 +24,11 @@ public class Mario extends Sprite {
 	public void jump() {
 		// JUMP!
 		moveByAmount(0,-10);
-		onGround = false;
 	}
 
 	public void act(ArrayList<Shape> obstacles) {
 		// FALL (and stop when a platform is hit)
+		boolean onGround = false;
 		for(Shape obstacle : obstacles) {
 			if(obstacle.contains(super.getCenterX(), super.getMaxY())) {
 				onGround = true;
